ba432a2b6f35b16895e611189582b5caf31d8c4c
rubyfeller <ruby.feller@student.fontys.nl>
2024-05-22 13:40:00 +0200

    Changed compose to map host.docker.internal

--- IntegrationTests/PostServiceTestFixture.cs
+++ IntegrationTests/PostServiceTestFixture.cs
@@ -62,8 +62,8 @@ public class PostServiceTestFixture : IDisposable, ICollectionFixture<PostServic
             .WithNetwork("bridge")
             .WithPortBinding(8080, true)
             .WithEnvironment("ConnectionStrings__PostsConn",
-                $"Server=172.17.0.1;Port={postgresPort};Database=postgres;User Id=postgres;Password=postgres;")
-            .WithEnvironment("RabbitMQHost", "172.17.0.1")
+                $"Server=host.docker.internal;Port={postgresPort};Database=postgres;User Id=postgres;Password=postgres;")
+            .WithEnvironment("RabbitMQHost", "host.docker.internal")
             .WithEnvironment("RabbitMQPort", rabbitmqPort.ToString())
             .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
             .DependsOn(postgresContainer)
