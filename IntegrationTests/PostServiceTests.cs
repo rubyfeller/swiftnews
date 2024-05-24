14f35cd94f7acee8e1367a9c2ddd538277b030d3
rubyfeller <93587021+rubyfeller@users.noreply.github.com>
2024-05-24 14:06:11 +0200

    Merge
    * Fix
    
    * Fix
    
    * Fix
    
    * Linux adaption
    
    * Linux adaption
    
    * Fix
    
    * New approach
    
    * Different approach
    
    * Fix
    
    * Test
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Update CI
    
    * Fix
    
    * Fix
    
    * Test
    
    * Changed compose to map host.docker.internal
    
    * Re-add check
    
    * Test runner
    
    * New approach: TestContainers/Docker networks
    
    * Improvements
    
    * Only test LikeService and MessageQueue
    
    * Fix ports, test
    
    * Test
    
    * Fix
    
    * Added logging
    
    * Extra logs
    
    * .
    
    * Added Docker login
    
    * More logs
    
    * Now building image in pipeline to prevent architecture issues linux/amd64
    
    * Readded all tests

--- IntegrationTests/PostServiceTests.cs
+++ IntegrationTests/PostServiceTests.cs
@@ -5,111 +5,111 @@ using Xunit;
 
 namespace IntegrationTests;
 
-// [Collection("PostServiceTests")]
-// public class PostServiceTests
-// {
-//     private readonly PostServiceTestFixture _fixture;
-//     private readonly Auth0Helper _auth0Helper;
+[Collection("PostServiceTests")]
+public class PostServiceTests
+{
+    private readonly PostServiceTestFixture _fixture;
+    private readonly Auth0Helper _auth0Helper;
 
-//     public PostServiceTests(PostServiceTestFixture fixture)
-//     {
-//         _fixture = fixture;
-//         _auth0Helper = _fixture.Auth0Helper;
-//     }
+    public PostServiceTests(PostServiceTestFixture fixture)
+    {
+        _fixture = fixture;
+        _auth0Helper = _fixture.Auth0Helper;
+    }
 
-//     [Fact]
-//     public async Task Can_Call_Get_Posts_Endpoint()
-//     {
-//         // Arrange
-//         var postgresContainer = await _fixture.StartPostgresContainerAsync();
-//         var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
-//         var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
+    [Fact]
+    public async Task Can_Call_Get_Posts_Endpoint()
+    {
+        // Arrange
+        var postgresContainer = await _fixture.StartPostgresContainerAsync();
+        var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
+        var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
 
-//         var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
+        var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
 
-//         var httpClient = new HttpClient();
-//         var requestUri = new UriBuilder(
-//             Uri.UriSchemeHttp,
-//             "localhost",
-//             postsServicePort,
-//             "api/posts"
-//         ).Uri;
+        var httpClient = new HttpClient();
+        var requestUri = new UriBuilder(
+            Uri.UriSchemeHttp,
+            "localhost",
+            postsServicePort,
+            "api/posts"
+        ).Uri;
 
-//         // Act
-//         var response = await httpClient.GetAsync(requestUri);
-//         response.EnsureSuccessStatusCode();
+        // Act
+        var response = await httpClient.GetAsync(requestUri);
+        response.EnsureSuccessStatusCode();
 
-//         // Assert
-//         var content = await response.Content.ReadAsStringAsync();
-//         Assert.Equal("[]", content);
-//     }
+        // Assert
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("[]", content);
+    }
 
-//     [Fact]
-//     public async Task Can_Post()
-//     {
-//         // Arrange
-//         var postgresContainer = await _fixture.StartPostgresContainerAsync();
-//         var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
-//         var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
+    [Fact]
+    public async Task Can_Post()
+    {
+        // Arrange
+        var postgresContainer = await _fixture.StartPostgresContainerAsync();
+        var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
+        var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
 
-//         var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
-//         var httpClient = new HttpClient();
-//         var requestUri = new UriBuilder(
-//             Uri.UriSchemeHttp,
-//             "localhost",
-//             postsServicePort,
-//             "api/posts"
-//         ).Uri;
+        var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
+        var httpClient = new HttpClient();
+        var requestUri = new UriBuilder(
+            Uri.UriSchemeHttp,
+            "localhost",
+            postsServicePort,
+            "api/posts"
+        ).Uri;
 
-//         var post = new { content = "Post integration test", author = "Test person" };
+        var post = new { content = "Post integration test", author = "Test person" };
 
-//         var token = await _auth0Helper.GetAuth0TokenAsync();
-//         httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var token = await _auth0Helper.GetAuth0TokenAsync();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-//         // Act
-//         var response = await httpClient.PostAsJsonAsync(requestUri, post);
-//         response.EnsureSuccessStatusCode();
+        // Act
+        var response = await httpClient.PostAsJsonAsync(requestUri, post);
+        response.EnsureSuccessStatusCode();
 
-//         // Assert
-//         var getResponse = await httpClient.GetStringAsync(requestUri);
-//         Assert.Contains("Post integration test", getResponse);
-//     }
+        // Assert
+        var getResponse = await httpClient.GetStringAsync(requestUri);
+        Assert.Contains("Post integration test", getResponse);
+    }
 
-//     [Fact]
-//     public async Task Can_Get_Specific_Post()
-//     {
-//         var postgresContainer = await _fixture.StartPostgresContainerAsync();
-//         var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
-//         var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
+    [Fact]
+    public async Task Can_Get_Specific_Post()
+    {
+        var postgresContainer = await _fixture.StartPostgresContainerAsync();
+        var rabbitmqContainer = await _fixture.StartRabbitMQContainerAsync();
+        var postServiceContainer = await _fixture.StartPostServiceContainerAsync(postgresContainer, rabbitmqContainer);
 
-//         var httpClient = new HttpClient();
-//         var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
+        var httpClient = new HttpClient();
+        var postsServicePort = postServiceContainer.GetMappedPublicPort(8080);
 
-//         var createPostUri = new UriBuilder(
-//             Uri.UriSchemeHttp,
-//             "localhost",
-//             postsServicePort,
-//             "api/posts"
-//         ).Uri;
+        var createPostUri = new UriBuilder(
+            Uri.UriSchemeHttp,
+            "localhost",
+            postsServicePort,
+            "api/posts"
+        ).Uri;
 
-//         var token = await _auth0Helper.GetAuth0TokenAsync();
-//         httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var token = await _auth0Helper.GetAuth0TokenAsync();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-//         var testPost = new { content = "Post integration test", author = "Test person" };
-//         var createResponse = await httpClient.PostAsJsonAsync(createPostUri, testPost);
-//         createResponse.EnsureSuccessStatusCode();
+        var testPost = new { content = "Post integration test", author = "Test person" };
+        var createResponse = await httpClient.PostAsJsonAsync(createPostUri, testPost);
+        createResponse.EnsureSuccessStatusCode();
 
-//         var getResponse = await httpClient.GetStringAsync(createPostUri);
-//         Assert.Contains("Post integration test", getResponse);
+        var getResponse = await httpClient.GetStringAsync(createPostUri);
+        Assert.Contains("Post integration test", getResponse);
 
-//         var getSpecificUri = new UriBuilder(
-//             Uri.UriSchemeHttp,
-//             "localhost",
-//             postsServicePort,
-//             "api/posts/1"
-//         ).Uri;
-//         var getSpecificResponse = await httpClient.GetStringAsync(getSpecificUri);
+        var getSpecificUri = new UriBuilder(
+            Uri.UriSchemeHttp,
+            "localhost",
+            postsServicePort,
+            "api/posts/1"
+        ).Uri;
+        var getSpecificResponse = await httpClient.GetStringAsync(getSpecificUri);
 
-//         Assert.Contains("Post integration test", getSpecificResponse);
-//     }
-// }
+        Assert.Contains("Post integration test", getSpecificResponse);
+    }
+}
