a9ffedba384af06b2d1687056a709e61d6402dd5
rubyfeller <ruby.feller@student.fontys.nl>
2024-06-12 09:39:35 +0200

    Added health checks

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -6,8 +6,6 @@ using Ocelot.Provider.Kubernetes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,20 +16,26 @@ string authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
 string audience = builder.Configuration["Auth0:Audience"] ?? string.Empty;
 
 builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(baseAuthenticationProviderKey, options =>
-        {
-            options.Authority = authority;
-            options.Audience = audience;
-        });
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(baseAuthenticationProviderKey, options =>
+{
+    options.Authority = authority;
+    options.Audience = audience;
+});
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
-builder.Services.AddOcelot(builder.Configuration).AddCacheManager(x => x.WithDictionaryHandle()).AddKubernetes();
+builder.Services.AddHealthChecks();
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(x => x.WithDictionaryHandle())
+    .AddKubernetes();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -41,8 +45,11 @@ if (app.Environment.IsDevelopment())
 app.UseRouting();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
+
+app.MapHealthChecks("/healthz");
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 await app.UseOcelot();
 
